4867b01f4d40cd97835ff468473406657df6cba6
Bryan Choe <bryan.choe88@gmail.com>
2016-10-06 14:02:12 -1000

    commit

--- server.js
+++ server.js
@@ -1,40 +1,71 @@
-const net = require('net');
-const fs = require('fs');
-let web;
-
-    fs.readFile('./public/index.html', (err, data) => {
-      if(err){
-        console.error(err);
-
-      }
-
-      web = data.toString();
-      console.log(web);
-    });
+const net = require ('net');
+const fs =require('fs');
 
 const server = net.createServer((request) => {
 
-  request.on('data', (data) => {
-    //status line
 
-    request.write('HTTP/1.1 200 OK');
-    request.write('\n');
-    request.write('\n');
-    request.write(web);
+  request.on('data', (data)=>{
+
+    console.log('GET REQUEST', data.toString());
+
+    let headerArray = data.toString().split(' ');
+    let requestMethod = headerArray[0];
+    let pathName = '.' + headerArray[1];
+    let type;
+
+    if(pathName === './'){
+      pathName = './public/index.html';
+    }
+
+    if(pathName.substring(pathName.length - 4) === 'html'){
+      type = 'html';
+    } else {
+      type = 'css';
+    }
+
+    fs.readFile( pathName, (err, data) => {
+      if(err){
+        console.log("error:", err);
+        fs.readFile( './public/404.html', (err, data) => {
+
+        let errorHeader = `HTTP/1.1 404 Not Found
+        Server: nginx/1.4.6 (Ubuntu)
+        Content-Type: text/html; charset=utf-8
+        Content-Length: ${data.toString().length}
+        Connection: keep-alive\n\n`;
+
+        request.write(errorHeader);
+        request.write(data.toString());
+
+        });
+
+      } else {
+
+        let header = `HTTP/1.1 200 OK
+        Server: nginx/1.4.6 (Ubuntu)
+        Content-Type: text/${type}; charset=utf-8
+        Content-Length: ${data.toString().length}
+        Connection: keep-alive\n\n`;
+
+        request.write(header);
+        request.write(data.toString());
+        request.end();
+      }
 
 
-    request.end();
+    });
+
+
   });
 
-  request.on('end', () => {
 
+  request.on('end', () => {
+    console.log('Connection Closed');
   });
 
 });
 
-//tell it what port to listen Opened
-server.listen({port:8080, host:'0.0.0.0'}, () => {
+server.listen({port: 8080}, ()=>{
   const address = server.address();
   console.log(`Opened server on ${address.port}`);
-
 });
